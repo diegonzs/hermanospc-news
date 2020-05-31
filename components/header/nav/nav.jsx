2652343380fc4312664853cd261699dfcf882ee3
Adriana Borjas <aborjasmontiel@gmail.com>
2020-05-31 18:05:03 -0400

    to much for now xD

--- components/header/nav/nav.jsx
+++ components/header/nav/nav.jsx
@@ -6,11 +6,12 @@ export const Nav = () => {
   return (
     <nav className={styles.nav}>
       <ul className={styles.list}>
-        <li><a href="#">Link-1</a></li>
-        <li><a href="#">Link-2</a></li>
-        <li><a href="#">Link-3</a></li>
-        <li><a href="#">Link-4</a></li>
-        <li><a href="#">Link-5</a></li>
+        <div className={styles.item}><li><a href="https://hermanospc.co/">News</a></li></div>
+        <div className={styles.item}><li><a href="https://hermanospc.co/calculator"><span>!</span> PC Calculator</a></li></div>
+        <div className={styles.item}><li><a href="https://hermanospc.co/free-to-play">F2P Games</a></li></div>
+        <div className={styles.item}><li><a href="https://hermanospc.co/wallapers">Wallpapers</a></li></div>
+        <div className={styles.item}><li><a href="https://hermanospc.co/">Shop</a></li></div>
+        <div className={styles.item}><li><a href="https://hermanospc.co/">Blog</a></li></div>
       </ul>
     </nav>
   )
--- components/header/social-media/social-media.jsx
+++ components/header/social-media/social-media.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
+import SVG from 'react-inlinesvg';
 // @ts-ignore
 import styles from './social-media.module.scss';
 
+const socialMediaList = [
+  {
+    link: 'https://instagram.com/hermanospc',
+    icon: '/images/social/facebook.svg'
+  },
+  {
+    link: 'https://facebook.com/hermanospc',
+    icon: '/images/social/facebook.svg'
+  },
+  {
+    link: 'https://twitter.com/hermanospc',
+    icon: '/images/social/facebook.svg'
+  },
+  {
+    link: 'https://tiktok.com/hermanospc',
+    icon: '/images/social/tiktok.svg'
+  },
+  {
+    link: '#',
+    icon: '/images/social/linkedin.svg'
+  },
+  {
+    link: 'https://discord.com/hermanospc',
+    icon: '/images/social/discord.svg'
+  },
+  {
+    link: '#',
+    icon: '/images/social/telegram.svg'
+  },
+] 
+
 export const SocialMedia = () => {
   return (
-    <div className={styles.social}>
       <ul className={styles.list}>
-        <li><a href="#">icon</a></li>
-        <li><a href="#">icon</a></li>
-        <li><a href="#">icon</a></li>
-        <li><a href="#">icon</a></li>
-        <li><a href="#">icon</a></li>
+        {socialMediaList.map((value) => (
+          <li className={styles.item}>
+            <a href={value.link}><SVG src={value.icon} /></a>
+          </li>
+        ))}
+        
       </ul>
-    </div>
   )
 }
