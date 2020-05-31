89eeff7defb1e226fe4aff17828187d76cdc412c
Adriana Borjas <aborjasmontiel@gmail.com>
2020-05-31 12:14:01 -0400

    footer styles adjusted

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -7,8 +7,8 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.list}>
-        <li><a href="https://hermanospc.co" target="_blank">About</a></li>
-        <li><a href="https://hermanospc.co" target="_blank">Laws</a></li>
+        <li><a href="https://hermanospc.co/legal" target="_blank">Legal</a></li>
+        <li><a href="https://hermanospc.co/ambassadors" target="_blank">Ambassadors</a></li>
       </div>
       <p>© 2020. Hermanos PC.</p>
     </footer>
