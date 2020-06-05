26e7dc3d0e17ef597e8817659284e97991641308
jordosgoite1 <jordosgoite@shokworks.io>
2020-06-04 22:15:27 -0400

    minor changes

--- components/lista_tag/list-tag.jsx
+++ components/lista_tag/list-tag.jsx
@@ -6,7 +6,7 @@ import styles from './list-tag.module.scss';
 
 /**
  * @typedef {Object} ListTagProps
- * @property {import('./tag/tag').TagProps[]} tags - the array of texts that would be in the tag
+ * @property {import('./tag/tag').TagProps[]} tags - List of tags to be render
  * @property {string} [gap] - space between the grid elements
  */
 /**
