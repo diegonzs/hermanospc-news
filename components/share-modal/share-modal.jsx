30bc2bd006cbcb1a385ada7160aead2f360b3c27
Jose Simancas <jsimancas@shokworks.io>
2020-06-06 19:25:26 -0400

    update

--- components/share-modal/share-modal.jsx
+++ components/share-modal/share-modal.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./share-modal.css";
-import { SocialMedia } from "components/header/social-media";
+import SVG from "react-inlinesvg";
+// @ts-ignore
+import facebookIcon from "../../public/images/social/facebook.svg";
+// @ts-ignore
+import whatsappIcon from "../../public/images/social/whatsapp.svg";
+// @ts-ignore
+import twitterIcon from "../../public/images/social/twitter.svg";
+// @ts-ignore
+import emailIcon from "../../public/images/social/envelope-open-text.svg";
+// @ts-ignore
+import linkedinIcon from "../../public/images/social/linkedin.svg";
 
 const ShareModal = () => {
   return (
@@ -11,8 +21,37 @@ const ShareModal = () => {
           <span className="cancel">Cancel</span>
         </a>
       </div>
-      <div>
-        <SocialMedia />
+      <div className="center">
+        <div className="margin-botton">
+          <SVG src={facebookIcon} />
+          <a href="https://facebook.com/hermanospc">
+            <span className="text">Facebook</span>
+          </a>
+        </div>
+        <div className="margin-botton">
+          <SVG src={whatsappIcon} />
+          <a href="#">
+            <span className="text">Whatsapp</span>
+          </a>
+        </div>
+        <div className="margin-botton">
+          <SVG src={twitterIcon} />
+          <a href="https://twitter.com/hermanospc">
+            <span className="text text2">Twitter</span>
+          </a>
+        </div>
+        <div className="margin-botton">
+          <SVG src={emailIcon} />
+          <a href="#">
+            <span className="text">Email</span>
+          </a>
+        </div>
+        <div className="margin-botton">
+          <SVG src={linkedinIcon} />
+          <a href="#">
+            <span className="text">LinkedIn</span>
+          </a>
+        </div>
       </div>
     </div>
   );
