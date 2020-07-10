cdabcb0c27609fd9d71aa98fc5284b9ef413a7e5
Digo Gonzalez <diego.ags04@gmail.com>
2020-07-10 15:31:31 -0400

    nothing

--- pages/jose.jsx
+++ pages/jose.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { JoseTitle } from 'components/jose-title';
-import { withApollo } from '../lib/apollo'
+import { withApollo } from '../lib/apollo';
+import { NewDetailContent } from 'components/news-detail/content';
 
 const Jose = () => {
-  const [name, setName] = useState('');
-  
-  useEffect(() => {
-    (async () => {
-      const res = await fetch('/api/hello');
-      const data = await res.json();
-      setName(data.name)
-    })()
-  }, [])
+	const [name, setName] = useState('');
 
-  return (
-    <>
-      <JoseTitle />
-      <p>{name}</p>
-    </>
-  )
-}
+	useEffect(() => {
+		(async () => {
+			const res = await fetch('/api/hello');
+			const data = await res.json();
+			setName(data.name);
+		})();
+	}, []);
 
+	return (
+		<>
+			{/* <JoseTitle /> */}
+			<NewDetailContent />
+		</>
+	);
+};
 
 export default withApollo({ ssr: false })(Jose);
