1c28eec337cecd0ff1e23df110dcca5ed764b31b
Digo Gonzalez <diego.ags04@gmail.com>
2020-09-06 14:41:40 -0400

    changed loader in news-details page

--- components/loading-page/loading-page.jsx
+++ components/loading-page/loading-page.jsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { Image } from 'components/image';
-import SVG from 'react-inlinesvg';
+// import { Image } from 'components/image';
+// import SVG from 'react-inlinesvg';
 //@ts-ignore
-import logo from '/images/brand/hermanospc-logo-small.svg';
+// import logo from '/images/brand/hermanospc-logo-small.svg';
 //@ts-ignore
 import styles from './loading-page.module.scss';
+import { Loader } from 'components/loader';
 
 export const LoadingPage = () => {
 	return (
 		<div className={styles.loadingContainer}>
-			<SVG src={logo} className={styles.image} cacheRequests={true} />
+			<Loader />
+			{/* <SVG src={logo} className={styles.image} cacheRequests={true} /> */}
 		</div>
 		// <h1>CARGANDO....</h1>
 	);
